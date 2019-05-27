0a290f139a192d79e2f8d7e501ca5b7504a239d1
Akashdeep Singh <akashthind007@gmail.com>
2019-05-27 09:58:52 -0400

    INITIAL COMMIT

---
+++ C0734733_AKASHDEEP_MAD5234_TEST2_QUESTION1/src/c0734733_akashdeep_mad5234_test2_question1/C0734733_AKASHDEEP_MAD5234_TEST2_QUESTION1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package c0734733_akashdeep_mad5234_test2_question1;
+
+/**
+ *
+ * @author Akashdeep Singh
+ */
+public class C0734733_AKASHDEEP_MAD5234_TEST2_QUESTION1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
